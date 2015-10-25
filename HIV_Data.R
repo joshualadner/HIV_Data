8b8436a76fbf8ced3e3fa376e226b910221b230f
Josh Ladner <joshua.ladner@yahoo.com>
2015-10-25 14:06:06 -0400

    Removed Log

--- HIV_Data.R
+++ HIV_Data.R
@@ -1,3 +1,3 @@
 acg.pcg.combined <- read.csv(file.choose(), header=TRUE)
-GSH.model = lmer(log(GSH) ~ Time + Region + (1|Subject) + (1|GSH..SD), data=acg.pcg.combined)
+GSH.model = lmer(GSH ~ Time + Region + (1|Subject) + (1|GSH..SD), data=acg.pcg.combined)
 summary(GSH.model)
